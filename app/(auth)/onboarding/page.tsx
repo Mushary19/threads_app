cc0edea4327a58a5e1865e189f53bb4d9ea71fd7
Mushary Rilwan <musharymushary72@gmail.com>
2024-03-22 11:58:41 +0530

    git updated

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -3,33 +3,34 @@ import { fetchUser } from "@/lib/actions/user.action"
 import { currentUser } from "@clerk/nextjs"
 import { redirect } from "next/navigation"
 
-async function Page () {
-
+async function Page() {
   // Currently logged in user
   const user = await currentUser()
 
   // Fetched  user from the database
-  const userInfo = {}
+  const userInfo = await fetchUser(user?.id)
 
-  // if (!userInfo?.onboarded) redirect('/onboarding')
+  if (!userInfo?.onboarded) redirect('/onboarding')
 
   const userData = {
     id: user?.id,
     objectId: userInfo?._id,
     username: userInfo?.username || user?.username,
-    name: userInfo?.name || user?.firstName || '',
-    bio: userInfo?.bio || '',
-    image: userInfo?.image || user?.imageUrl
+    name: userInfo?.name || user?.firstName || "",
+    bio: userInfo?.bio || "",
+    image: userInfo?.image || user?.imageUrl,
   }
 
   return (
     <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
-        <h1 className='head-text'>Onboarding...</h1>
-        <p className="mt-3 text-base-regular text-light-2">Complete your profile now to use Threads</p>
+      <h1 className="head-text">Onboarding...</h1>
+      <p className="mt-3 text-base-regular text-light-2">
+        Complete your profile now to use Threads
+      </p>
 
-        <section className="mt-9 bg-dark-2 p-10 rounded">
-          <AccountProfile user={userData} btnTitle='Continue' />
-        </section>
+      <section className="mt-9 bg-dark-2 p-10 rounded">
+        <AccountProfile user={userData} btnTitle="Continue" />
+      </section>
     </main>
   )
 }
